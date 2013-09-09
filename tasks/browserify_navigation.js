64e86097a1bd75d53d1a1ea064cbf1739caa8b52
Matt Dean <matt@trabian.com>
2013-09-09 09:56:10 -0400

    Gracefully handle missing Redis server

--- tasks/browserify_navigation.js
+++ tasks/browserify_navigation.js
@@ -17,34 +17,46 @@ module.exports = function(grunt) {
 
     var opts = this.options();
 
+    opts.clientOptions = opts.clientOptions || {};
+
+    opts.clientOptions.max_attempts = opts.max_attempts || 1;
+
     var redis = require('redis');
 
     var client = redis.createClient(opts.port, opts.host, opts.clientOptions);
 
-    var namespace = opts.namespace || 'browserify';
-
-    grunt.log.ok('Redis client started using server at', [client.host, client.port].join(':'));
-
-    grunt.event.removeAllListeners('browserify.dep');
-
-    grunt.event.on('browserify.dep', function (dep) {
-
-      var id, pathName;
-
-      for (id in dep.deps) {
-        pathName = dep.deps[id];
-        client.hset(namespace + ":" + dep.id, id, pathName);
-      }
-
+    client.on("error", function (e) {
+      grunt.log.warn('Dependencies will not be saved to Redis do to a connection error', e);
     });
 
-    process.on('exit', function () {
-      client.quit();
-      grunt.log.ok('Redis client closed');
-    });
+    client.on("ready", function(e) {
 
-    process.on('SIGINT', function () {
-      process.exit();
+      var namespace = opts.namespace || 'browserify';
+
+      grunt.log.ok('Redis client started using server at', [client.host, client.port].join(':'));
+
+      grunt.event.removeAllListeners('browserify.dep');
+
+      grunt.event.on('browserify.dep', function (dep) {
+
+        var id, pathName;
+
+        for (id in dep.deps) {
+          pathName = dep.deps[id];
+          client.hset(namespace + ":" + dep.id, id, pathName);
+        }
+
+      });
+
+      process.on('exit', function () {
+        client.quit();
+        grunt.log.ok('Redis client closed');
+      });
+
+      process.on('SIGINT', function () {
+        process.exit();
+      });
+
     });
 
   });
